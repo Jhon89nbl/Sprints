0fff4846db202529b708d311bb35a54eab857257
F E C <theseb242@gmail.com>
2021-09-27 15:59:23 -0500

    validacionTel

---
+++ js/form.js
@@ -0,0 +1,9 @@
+
+
+function checkTelefono(valor){
+
+    var valor = document.getElementById("telefono");
+    if(valor == null || valor.length == 0){
+        alert("El campo de telefono no puede quedar vacio");
+    }
+}
