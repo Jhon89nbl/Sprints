aacd38ecc0224ab185b6cc530a2d906798f6ddea
F E C <theseb242@gmail.com>
2021-10-01 16:52:32 -0500

    funcion telefono full

--- js/form.js
+++ js/form.js
@@ -1,9 +1,21 @@
+// funcion macro que agrupe las funciones de registro
+// la funcion macro seria la que se coloque en el onclick registro(para buenas practicas)
+// no requerido por el bot
+
+
+
+
 
 
 function checkTelefono(valor){
 
-    var valor = document.getElementById("telefono");
+    var valor = document.getElementById("telefono").value;
     if(valor == null || valor.length == 0){
         alert("El campo de telefono no puede quedar vacio");
+        return;
     }
 }
+
+
+module.export = checkTelefono;
+
