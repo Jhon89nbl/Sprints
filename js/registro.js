dce39c1875865f2d2870da9e5863a57ecb46b73c
Maria Ana Lucia piñeros Zambrano <jhon89nbl@gmail.com>
2021-10-06 15:38:47 -0500

    se crea archivo registro.js

---
+++ js/registro.js
@@ -0,0 +1,28 @@
+/***Arreglo de objetos y crear registro Johan Sebastian Harrison */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/***Ordenar el contenido por el nombre y filtro de registro por correo gmail.com Jhon y Jorge */
+
