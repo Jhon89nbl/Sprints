9ab29b164271238ae62dddeb91cf86a9c7e65090
Maria Ana Lucia piñeros Zambrano <jhon89nbl@gmail.com>
2021-10-08 14:37:22 -0500

    Se agrega funcion ordenar y filtrar

--- js/registro.js
+++ js/registro.js
@@ -26,3 +26,26 @@
 
 /***Ordenar el contenido por el nombre y filtro de registro por correo gmail.com Jhon y Jorge */
 
+function ordenarArreglo(arreglo){
+    registros.sort((a,b) =>{
+        if(a.nombre > b.nombre) return 1;
+        if(a.nombre < b.nombre) return -1;
+        
+        return 0;
+    });
+    console.log(registros);
+    return registros;
+}
+
+function filtrarCorreo(arreglo){
+    let temp= []
+    registros.forEach(element => {
+        if(element.correo.indexOf("@gmail.com")> -1){
+            temp.push(element);
+        }
+    });
+    return temp;
+}
+
+module.exports.filtrarCorreo = filtrarCorreo;
+module.exports.ordenarArreglo = ordenarArreglo;
